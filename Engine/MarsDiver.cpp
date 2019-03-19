028b9e227b4c3a6d9dad5b9f7c0c8f14a45c4706
Aescosaurus <michaelbuslik@hotmail.com>
2019-03-18 20:08:19 -0600

    Mars diver bounces off the edges of the screen.

--- Engine/MarsDiver.cpp
+++ Engine/MarsDiver.cpp
@@ -8,17 +8,16 @@ MarsDiver::MarsDiver( const Vec2& pos )
 void MarsDiver::Update( const Vec2& playerPos,float dt )
 {
 	const Vec2 diffVec = playerPos - pos;
+	const float distSq = diffVec.GetLengthSq<float>();
 
 	retarget.Update( dt );
 
-	if( diffVec.GetLengthSq<float>() <=
-		activationRange * activationRange &&
+	if( distSq <= activationRange * activationRange &&
 		retarget.IsDone() )
 	{
 		angle = diffVec.GetAngle<float>();
 
-		if( diffVec.GetLengthSq<float>() <=
-			jumpRange * jumpRange )
+		if( distSq <= jumpRange * jumpRange )
 		{
 			retarget.Reset();
 
@@ -30,6 +29,22 @@ void MarsDiver::Update( const Vec2& playerPos,float dt )
 	pos += vel * dt;
 	vel -= ( ( vel * ( 1.0f - velDecay ) ) *
 		( retarget.GetPercent() / 5.0f ) ) * dt;
+
+	// Bounce off the edges of the screen.
+	if( pos.x - float( radius ) < 1.0f ||
+		pos.x + float( radius ) >
+		float( Graphics::ScreenWidth - 1 ) )
+	{
+		vel.x *= -1.0f;
+		pos += vel * dt;
+	}
+	if( pos.y - float( radius ) < 1.0f ||
+		pos.y + float( radius ) >
+		float( Graphics::ScreenHeight - 1 ) )
+	{
+		vel.y *= -1.0f;
+		pos += vel * dt;
+	}
 }
 
 void MarsDiver::Draw( Graphics& gfx ) const
