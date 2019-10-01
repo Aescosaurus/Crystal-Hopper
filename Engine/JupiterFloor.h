bcda3f5861251d97ec5af19386bbeeeb9e951adf
Aescosaurus <michaelbuslik@hotmail.com>
2019-10-01 07:02:50 -0600

    Made platform particle spawning a little less aggressive.

--- Engine/JupiterFloor.h
+++ Engine/JupiterFloor.h
@@ -16,11 +16,16 @@ public:
 
 	void Update( std::vector<std::unique_ptr<Explosion>>& particles,float dt )
 	{
-		particles.emplace_back( std::make_unique<Explosion>(
-			lines[int( Random{ 0,int( lines.size() ) - 1 } )]
-			.GetRandSpot(),
-			Explosion::Type::ParticleDissipate,
-			Explosion::Behavior::Falling
-			) );
+		if( int( Random{ 0,100 } ) < particleSpawnChance )
+		{
+			particles.emplace_back( std::make_unique<Explosion>(
+				lines[int( Random{ 0,int( lines.size() ) - 1 } )]
+				.GetRandSpot(),
+				Explosion::Type::ParticleDissipate,
+				Explosion::Behavior::Falling
+				) );
+		}
 	}
+private:
+	static constexpr int particleSpawnChance = 30;
 };
